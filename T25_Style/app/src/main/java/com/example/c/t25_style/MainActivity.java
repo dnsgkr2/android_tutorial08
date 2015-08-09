c22389fd84faf17fd202962b4aadd58c512218c1
dnsgkr2 <dnsgkr2@naver.com>
2015-08-09 17:57:01 +0900

    style

---
+++ T25_Style/app/src/main/java/com/example/c/t25_style/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.c.t25_style;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    TextView selectedTextView, workingTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        selectedTextView = (TextView)findViewById(R.id.selectedTextView);
+        workingTextView = (TextView)findViewById(R.id.workingTextView);
+//        button1 = (Button)findViewById(R.id.button1);
+//        button2 = (Button)findViewById(R.id.button2);
+//        buttonEnter = (Button)findViewById(R.id.enterButton);
+
+        View.OnClickListener numberButtonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Button b = (Button)v;
+                String str = workingTextView.getText().toString();
+                str+= b.getText().toString();
+                workingTextView.setText(str);
+            }
+        };
+
+//        button1.setOnClickListener(numberButtonListener);
+//        button2.setOnClickListener(numberButtonListener);
+
+
+
+
+        TableLayout tableLayout = (TableLayout)findViewById(R.id.tableLayout);
+        for(int i=2; i<tableLayout.getChildCount(); i++) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(i);
+
+            int number = 1;
+            for (int k = 0; k < tableRow.getChildCount(); k++) {
+                Button button = (Button) tableRow.getChildAt(k);
+                button.setText("" + number);
+                button.setOnClickListener(numberButtonListener);
+                number++;
+            }
+            int bottomCount = tableLayout.getChildCount();
+            TableRow bottomRow = (TableRow) tableLayout.getChildAt(bottomCount - 1);
+            Button deleteButton = (Button) bottomRow.getChildAt(0);
+            deleteButton.setText(" Áö¿ò");
+            deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    workingTextView.setText("0");
+                }
+            });
+
+            Button zeroButton = (Button) bottomRow.getChildAt(1);
+            zeroButton.setText("0");
+            zeroButton.setOnClickListener(numberButtonListener);
+
+            Button enterButton = (Button) bottomRow.getChildAt(2);
+            enterButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    selectedTextView.setText(workingTextView.getText().toString());
+                }
+            });
+          }
+        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
