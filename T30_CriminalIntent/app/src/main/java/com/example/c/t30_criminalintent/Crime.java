cb43acf6beccbee6a44784dc87f5413b60468619
dnsgkr2 <dnsgkr2@naver.com>
2015-08-22 17:55:26 +0900

    CriminalIntent

---
+++ T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/Crime.java
@@ -0,0 +1,56 @@
+package com.example.c.t30_criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by c on 2015-08-22.
+ */
+public class Crime {
+    private String mTitle;
+    private UUID mld;
+    private Date mDate;
+    private boolean mSolved;
+
+
+
+
+
+    public Crime(){
+        mld = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public UUID getld() {
+        return mld;
+    }
+
+    public void setld(UUID ld) {
+        this.mld = ld;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+}
