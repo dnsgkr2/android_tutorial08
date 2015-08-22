845d362b378f1877bd5c64c312f8e49a6a72f468
dnsgkr2 <dnsgkr2@naver.com>
2015-08-22 12:50:14 +0900

    webView

---
+++ T26_WebView/app/src/main/java/com/example/c/t26_webview/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.c.t26_webview;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+
+public class MainActivity extends ActionBarActivity {
+    WebView webView;
+    ProgressBar progressBar;
+
+
+    WebChromeClient webChromeClient = new WebChromeClient(){
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+
+            if(newProgress == 100)
+                progressBar.setVisibility(View.INVISIBLE);
+            else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(newProgress);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+       // webView.loadUrl("http://www.daum.net");
+        webView.loadUrl("file:///android_asset/hello.html");
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        webView.setWebChromeClient(webChromeClient);
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        if(webView.canGoBack())
+            webView.goBack();
+        else
+            finish();
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
