d47590942fae36592f7732962af01cc2dd34a4be
dnsgkr2 <dnsgkr2@naver.com>
2015-08-23 11:39:45 +0900

    design supportlibrary

--- T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/CrimeLab.java
+++ T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/CrimeLab.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 /**
  * Created by c on 2015-08-22.
+ * 어레이리스트를 담을수 있는 클래스  싱글턴... 스태틱특성이용해서 생성(프로그램에서 하나만 존재...
+ * 프로그램 실행될때 미리 한번만 로딩
  */
 public class CrimeLab {
     private ArrayList<Crime> mCrimes;
@@ -19,6 +21,12 @@ public class CrimeLab {
 
     public CrimeLab(){
         mCrimes = new ArrayList<Crime>();
+         for(int i =0; i<100; i++){
+             Crime c = new Crime();
+             c.setTitle("범죄 " + i);
+             c.setSolved(i%2==0);
+             mCrimes.add(c);
+         }
     }
 
     public ArrayList<Crime> getCrimes(){
---
+++ T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/CrimeListFragment.java
@@ -0,0 +1,23 @@
+package com.example.c.t30_criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+
+import java.util.ArrayList;
+
+/**
+ * Created by c on 2015-08-23.
+ * 리스트뷰를 담고있는 프래그먼트
+ */
+public class CrimeListFragment extends ListFragment{
+    private ArrayList<Crime> mCrimes;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActivity().setTitle("범죄 리스트");
+        mCrimes = CrimeLab.get().getCrimes();
+
+    }
+}
